45e9cc14d400843172e3cc094a24be80091b9706
Peter Bittner <django@bittner.it>
2025-06-22 12:30:14 +0200

    Refactor testcase implementation to reduce mental overhead
    Inheritance can make code cleaner by applying the DRY principle, but
    occasionally it comes at a price.  With some limited code duplication and
    composition instead of inheritance we can reduce the mental energy needed
    to understand the implementation.

--- behave_django/testcase.py
+++ behave_django/testcase.py
@@ -1,71 +1,83 @@
+"""
+TestCase classes to prevent unit tests from being executed.
+
+We want to run Behave under the hood instead of unit tests.
+We do this by disabling the setup, teardown and fixture loading methods
+of Django's TestCase classes. This way we can reuse what the Django
+project adds to Python's unittest setup, yet having full total control
+over the test execution.
+
+Django 5.2 changed ``_pre_setup`` and ``_fixture_setup`` to classmethods,
+hence the conditional handling below.
+See https://github.com/django/django/commit/8eca3e9b
+"""
+
 import django
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test.testcases import TestCase
 
-
-class PreventTestSetupMixin:
-    """Prevents the TestCase from executing its setup and teardown methods.
-
-    This mixin overrides the test case's ``_pre_setup`` and ``_post_teardown``
-    methods in order to prevent them from executing when the test case is
-    instantiated.  We do this to have total control over the test execution.
-
-    Django 5.2 changed ``_pre_setup`` and ``_fixture_setup`` to classmethods,
-    hence the conditional handling below.
-    See https://github.com/django/django/commit/8eca3e9b
-    """
-
-    def _post_teardown(self, run=False):
-        if run:
-            super()._post_teardown()
-
-    def runTest(self):
-        pass
-
-
 if django.VERSION < (5, 2):
 
-    class BehaviorDrivenTestMixin(PreventTestSetupMixin):
+    class PreventTestExecutionMixin:
         def _pre_setup(self, run=False):
             if run:
                 super()._pre_setup()
+
+        def _post_teardown(self, run=False):
+            if run:
+                super()._post_teardown()
+
+        def runTest(self):
+            pass
 
     class PreventFixturesMixin:
         def _fixture_setup(self):
             pass
 
+        def _fixture_teardown(self):
+            pass
+
 else:  # Django 5.2 introduced some classmethods
 
-    class BehaviorDrivenTestMixin(PreventTestSetupMixin):
+    class PreventTestExecutionMixin:
         @classmethod
         def _pre_setup(cls, run=False):
             if run:
                 super()._pre_setup()
+
+        def _post_teardown(self, run=False):
+            if run:
+                super()._post_teardown()
+
+        def runTest(self):
+            pass
 
     class PreventFixturesMixin:
         @classmethod
         def _fixture_setup(cls):
             pass
 
+        def _fixture_teardown(self):
+            pass
 
-class BehaviorDrivenTestCase(BehaviorDrivenTestMixin, StaticLiveServerTestCase):
+
+class BehaviorDrivenTestCase(PreventTestExecutionMixin, StaticLiveServerTestCase):
     """Test case attached to the context during behave execution.
 
     This test case prevents the regular tests from running.
     """
 
 
-class ExistingDatabaseTestCase(PreventFixturesMixin, BehaviorDrivenTestCase):
+class ExistingDatabaseTestCase(
+    PreventFixturesMixin, PreventTestExecutionMixin, StaticLiveServerTestCase
+):
     """Test case used for the --use-existing-database setup.
 
     This test case prevents fixtures from being loaded to the database in use.
     """
 
-    def _fixture_teardown(self):
-        pass
 
-
-class DjangoSimpleTestCase(BehaviorDrivenTestMixin, TestCase):
+class DjangoSimpleTestCase(PreventTestExecutionMixin, TestCase):
     """Test case attached to the context during behave execution.
 
     This test case uses `transaction.atomic()` to achieve test isolation
